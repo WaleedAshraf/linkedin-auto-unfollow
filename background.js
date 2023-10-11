dca2d0f56af1be3015a1ea368ed241742f00e8bf
Waleed Ashraf <waleedashraf@outlook.com>
2023-10-11 10:37:57 +0500

    remove status check from tab listner

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 const profilePageRegex = /https:\/\/\w{1,3}.linkedin.com\/in/
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (profilePageRegex.test(changeInfo.url) && changeInfo.status === 'complete') {
+  if (profilePageRegex.test(changeInfo.url)) {
     chrome.tabs.sendMessage(tabId, {
       message: "clicked",
       url: changeInfo.url,
